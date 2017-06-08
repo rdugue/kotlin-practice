9b88e967ab0c008128fd9ac08ba0b6c0a390342f
Ralph Dugue <rdugue1@gmail.com>
2017-06-08 04:08:05 -0400

    Updated accumulate solution to follow rules better.

--- accumulate/src/main/kotlin/Accumulate.kt
+++ accumulate/src/main/kotlin/Accumulate.kt
@@ -1,3 +1,11 @@
 package Accumulate 
 
-fun <T, R> accumulate(input: List<T>, op: (T) -> R): List<R> = input.map(op)
+fun <T, R> accumulate(input: List<T>, op: (T) -> R): List<R> {
+    var output = mutableListOf<R>()
+
+    for (i in input) {
+        output.add(op(i))
+    }
+
+    return output
+}

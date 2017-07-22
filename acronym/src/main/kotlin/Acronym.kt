3ad8de45e0b8ce271edf8dfd00ff100e5647f038
Ralph Dugue <rdugue1@gmail.com>
2017-07-21 22:13:19 -0400

    Updated acronym solution

--- acronym/src/main/kotlin/Acronym.kt
+++ acronym/src/main/kotlin/Acronym.kt
@@ -3,16 +3,15 @@ package Acronym
 fun generate(phrase: String): String? {
     val words = phrase.split(" ", "-")
     var result: String? = null
-
     for (word in words) {
-        result?.plus(word[0]) ?: result = "$word"
+        result = result?.plus(word[0].toUpperCase()) ?: "${word[0]}"
+        println(result)
         val last = word.length - 1
         for (i in word.slice(2..last).indices) {
             if (i < 1) {
                 if (word[i].isUpperCase()) result.plus(word[i])
             } else {
-                if (word[i-1].isUpperCase() || word[i].isLowerCase()) continue
-                result.plus(word[i])
+                if (word[i-1].isLowerCase() && word[i].isUpperCase()) result.plus(word[i])
             }
         }
     }
